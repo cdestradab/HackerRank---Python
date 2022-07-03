a8f81f29db0e33cf3bf972fd6b396c1bdaef0926
Cristian Estrada <59891802+cdestradab@users.noreply.github.com>
2022-07-03 21:09:55 +0000

    challenge8 done

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-englishNews = '9'
-englishNewsSet = set("1 2 3 4 5 6 7 8 9".split(sep=' '))
-frenchNews = '9'
-frenchNewsSet = set("10 1 2 3 11 21 55 6 8".split(sep=' '))
+import challenge8 as ch8
 
-def howManyForBoth(set1, set2):
-  print(set1)
-  print(set2)
-  
-  inters = set1.intersection(set2);
-  return(len(inters));
-  
-print(howManyForBoth(englishNewsSet,frenchNewsSet));
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+print(ch8.create3dGridRestricted(x,y,z,n));
