da3755d011b5254202b926e753bc0e2e546ec2eb
Cristian Estrada <59891802+cdestradab@users.noreply.github.com>
2022-07-03 05:05:13 +0000

    challenge6 done

--- challenge5.py
+++ challenge5.py
@@ -1,3 +1,4 @@
+#HackerRank
 def is_leap(year):
     leap = False
     
@@ -12,3 +13,11 @@ def is_leap(year):
 
 year = int(input())
 print(is_leap(year))
+
+#Testing
+
+'''import challenge5 as ch5;
+years = [2000,2400,1800, 1900, 2100, 2200, 2300,2500,1990];
+
+for i in years:
+  print(ch5.is_leap(i));'''
