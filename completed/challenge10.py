a9e56a91fe634017b6dad685c6bfda02276b9b45
Cristian Estrada <59891802+cdestradab@users.noreply.github.com>
2022-07-06 05:21:50 +0000

    challenge10 done

---
+++ completed/challenge10.py
@@ -0,0 +1,27 @@
+def printSeconds(lista):
+  scores = [];
+  for a in lista:
+    scores.append(a[1]);
+
+  lowToHighScores = sorted(list(set(scores)));
+
+  secondLowestStudents = [];
+  for a in lista:
+    if a[1] == lowToHighScores[1]:
+      secondLowestStudents.append(a[0]);
+
+  secondLowestStudents = sorted(secondLowestStudents)
+  for s in secondLowestStudents:
+    print(s);
+
+#HackerRank
+'''if __name__ == '__main__':
+    students = [];
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name, score]); #revisar
+    printSeconds(students);'''
+
+#Testing:
+'''score = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]'''
