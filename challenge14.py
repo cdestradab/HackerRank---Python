30eb0cdf5d5d33318bc51db86de1cabcb7c63c17
Cristian Estrada <59891802+cdestradab@users.noreply.github.com>
2022-07-16 05:32:14 +0000

    challenge14.py

---
+++ challenge14.py
@@ -0,0 +1,14 @@
+def swap_case(s):
+    return s.swapcase();
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+
+#Nota:
+'''
+Los strings de python tienen una cantidad considerable de métodos útiles que permiten interactuar de distintas formas con las variables. Revisar la documentación.
+
+Mas info: https://www.w3schools.com/python/python_ref_string.asp
+'''
