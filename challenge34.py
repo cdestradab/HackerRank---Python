b1d44b368dcfcd4b13ed9ee2f72d65bfd5f1f442
Cristian Estrada <59891802+cdestradab@users.noreply.github.com>
2022-08-10 03:14:18 +0000

    challenge34 DONE

---
+++ challenge34.py
@@ -0,0 +1,12 @@
+#Python> Date and Time > Calendar Module
+#https://www.hackerrank.com/challenges/calendar-module/problem?isFullScreen=true
+import calendar
+
+MM, DD, YY = map(int, input().split(" "));
+
+print(calendar.day_name[calendar.weekday(YY,MM,DD)].upper());
+
+#NOTAS:
+'''
+Importante siempre leer el orden de los parámetros antes de usar el método, para no tener que adivinar en que orden ponerlos valores.
+'''
